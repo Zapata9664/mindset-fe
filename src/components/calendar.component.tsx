0be6248eed8d5010f6d51eb8ae5c4cdb0ff5923a
Zapata9664 <valentinazapatajaramillo@gmail.com>
2023-03-22 13:59:13 -0500

    Calendar

--- src/components/calendar.component.tsx
+++ src/components/calendar.component.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { LocalizationProvider, deDE, DatePicker, DateCalendar } from '@mui/x-date-pickers';
+import React, { useState} from 'react'
+import { LocalizationProvider, DateCalendar, DateCalendarProps } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
+const adapter = new AdapterDayjs({});
 
-export const Calendar = () => {
+export const Calendar = (props: JSX.IntrinsicAttributes & DateCalendarProps<Date>) => {
+    const firstAvailableSlotDay = adapter.date(new Date(2024, 10, 10))
+    const [calendarValue, setCalendarValue] = useState<Date | null>(new Date());
+
+    const onChange = (newValue:Date | null) => {
+        setCalendarValue(newValue)
+        console.log('hola' + newValue)
+    }
+
     return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar />
+        <DateCalendar onChange={onChange} value={calendarValue}></DateCalendar>
     </LocalizationProvider>
     )
 }
--- src/pages/lading.tsx
+++ src/pages/lading.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Calendar } from '../components/calendar.component'
+import { LocalizationProvider, DateCalendar, DateCalendarProps } from '@mui/x-date-pickers';
+
+
 
 export const Lading = () => {
+
     return (
-        <Calendar/>
+        <div>
+            <Calendar/>
+        </div>
     )
 };
