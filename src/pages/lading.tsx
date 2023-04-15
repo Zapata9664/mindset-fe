bf30496c8228c7cd3950b5704dc5866fcddcf93f
Zapata9664 <valentinazapatajaramillo@gmail.com>
2023-04-15 12:28:13 -0500

    lading

--- src/pages/lading.tsx
+++ src/pages/lading.tsx
@@ -31,20 +31,36 @@ export const Lading = () => {
     }
 
     return (
-        <div className="flex bg-[url('/src/assets/backgroundImageLading.png')] bg-cover">
-            <div className='p-20 m-24 border-gray-900 space-y-6'>
-                <h1 className='text-teal-700 font-sans text-lg'><b>PLEASE SELECT A DAY</b></h1>
-                <Calendar onChange={onChange} className='rounded-xl bg-teal-50 border-2 border-gray-400' />
-            </div>
-            <div className='flex-row py-72 px-20'>
-                <h1 className='text-teal-700 font-sans text-lg py-4'><b>PLEASE SELECT A HOUR</b></h1>
-                <div className='flex space-x-6'>
-                    {buttons.map((days: JSX.Element, index: number) => (
-                        <Button onClick={() => onChangeHour(index)} sx={{ backgroundColor: '#8AA6A3', color: 'white', borderColor: '#8AA6A3' }} variant='outlined' className='border-teal-500' key={index} data-index={index}>{days}</Button>
-                    ))}
+        <div className="flex-col bg-[url('/src/assets/backgroundImageLading.png')] bg-cover">
+            <div className="grid grid-cols-2 gap-2 ">
+
+
+                <div className='p-20 m-24 space-y-6 '>
+                    <h1 className='text-teal-700 font-sans text-lg'><b>PLEASE SELECT A DAY</b></h1>
+                    <Calendar onChange={onChange} className='rounded-xl bg-teal-50 border-2 border-gray-400  w-96' />
                 </div>
-                <div className='py-5'>
-                    <Button onClick={takeDate} sx={{ backgroundColor: '#10403b', color: 'white', borderColor: '#8AA6A3' }} variant='outlined'>TAKE YOUR DATE</Button>
+
+
+                <div className='flex-col py-72'>
+                    <h1 className='text-teal-700 font-sans text-lg py-2'><b>PLEASE SELECT A HOUR</b></h1>
+                    <div className='flex space-x-6 p-4 py-2 px-44'>
+                        {buttons.map((days: JSX.Element, index: number) => (
+                            <Button onClick={() => onChangeHour(index)} sx={{ backgroundColor: '#8AA6A3', color: 'white', borderColor: '#8AA6A3' }} variant='outlined' key={index} data-index={index}>{days}</Button>
+                        ))}
+                    </div>
+                    <div className='py-6'>
+                        <Button onClick={takeDate} sx={{ backgroundColor: '#10403b', color: 'white', borderColor: '#8AA6A3' }} variant='outlined'>TAKE YOUR DATE</Button>
+                    </div>
+                </div>
+
+
+            </div>
+            <div className='p-14'>
+                <div className='p-7 '>
+                    <h1 className='text-teal-700 font-sans text-lg '><b>OUR PACKAGES</b></h1>
+                </div>
+                <div>
+                    <Packages></Packages>
                 </div>
             </div>
         </div>
